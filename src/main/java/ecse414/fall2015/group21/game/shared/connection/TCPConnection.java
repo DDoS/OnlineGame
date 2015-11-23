65159b81b7ca8e825464e7046f5459e0464b8717
AidanPetit <aidan.petit@mail.mcgill.ca>
2015-11-22 23:43:25 -0500

    Added TCPConnectionHandle, mostly the same as UDP version. Continued work on TCPCOnnection, send() is still not finished but other methods are implemented.

--- src/main/java/ecse414/fall2015/group21/game/shared/connection/TCPConnection.java
+++ src/main/java/ecse414/fall2015/group21/game/shared/connection/TCPConnection.java
@@ -2,7 +2,6 @@ package ecse414.fall2015.group21.game.shared.connection;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import io.netty.channel.*;
 
 import ecse414.fall2015.group21.game.shared.codec.TCPDecoder;
 import ecse414.fall2015.group21.game.shared.codec.TCPEncoder;
@@ -21,18 +20,70 @@ import java.net.InetSocketAddress;
 
 
 /**
- *  Testing GIT commit to prevent error.
+ *  Represents a TCP connection. It holds the addresses (IP and port) of both sides of the connection.
+ *  Responcible for reading and sending TCP packets
+ *
  *  Aidan and Bryce
  */
 public class TCPConnection implements Connection {
     private Address local;
     private final Address remote;
     private final Channel channel;
+    private final Queue<Packet.TCP> received = new LinkedList<>();
 
+    private boolean managed;
+    private EventLoopGroup group = null;
+    private TCPConnectionHandler handler = null;
+
+    /**
+     * Creates a new TCP connection. No manager is needed for this single connection.
+     *
+     * @param local The local address of the connection (port)
+     * @param remote The remote address to send to (ip and port)
+     */
+    public TCPConnection(Address local, Address remote){
+        this.local = local;
+        this.remote = remote;
+        group = new NioEventLoopGroup();    //allows channels to be processed
+        handler = new TCPConnectionHandler();
+
+        //Attempt to create a new channel, throws exception on failure
+        try{
+            Bootstrap startUp = new Bootstrap();
+
+            //Bootstrap documentation says to use connect() methods instead of .bind() for TCP
+            channel = startUp.group(group)
+                    .channel(NioDatagramChannel.class)    //Netty IO datagram channel
+                    .handler(handler)
+                    .localAddress(local.getPort())                      //Add local port to the channel
+                    .connect(remote.asInetAddress(), remote.getPort())  //connect the remote Address to the channel
+                    .channel()
+                    ;
+
+
+        } catch (Exception exception) {
+            group.shutdownGracefully();
+            throw new RuntimeException("Failed to create connection at " + local, exception);
+        }
+
+        System.out.println("TCP Connection opened at " + local.toString());
+        managed = false;    //this connection has no manager
+    }
+
+    /**
+     * Creates a TCP connection from an existing Channel.
+     * This connection will use the channel to send but will not read the channel.
+     * Reading the channel is handled by TCP connection manager by calling handOff(Packet.TCP)
+     *
+     * @param local The local address of the connection (port)
+     * @param remote The remove address to be sent to (ip and port)
+     * @param ch The existing Channel used for sending
+     */
     public TCPConnection(Address local, Address remote, Channel ch) {
         this.local = local;
         this.remote = remote;
         this.channel = ch;
+        managed = true;
     }
 
     @Override
@@ -40,6 +91,13 @@ public class TCPConnection implements Connection {
         return remote;
     }
 
+    /**
+     * Send all messages
+     *
+     * Not finished
+     *
+     * @param queue The messages to send
+     */
     @Override
     public Address getLocal() {
         return local;
@@ -80,11 +138,11 @@ public class TCPConnection implements Connection {
     public void receive(Queue<? super Message> queue) {
         // TODO: read ByteBufs from channel, use Packet.TCP.FACTORY.newInstance create and add to this queue
 
-        //Is this channel already open? or do we need to open a new channel?
-
-
-        final Queue<Packet.TCP> received = new LinkedList<>();
-
+        // If this connection doesnt have a manager, the handler takes care of reading the packets
+        if (!managed) {
+            handler.readPackets(received);
+        }
+        //Decode packets using factory and add them to the queue
         for (Packet.TCP packet : received) {
             TCPDecoder.INSTANCE.decode(packet, remote, queue);
         }
@@ -99,5 +157,10 @@ public class TCPConnection implements Connection {
             channel.close();
 
         }
+    }
+
+    //HandOff method for TCP connection manager to read the channel
+    void handOff(Packet.TCP received) {
+        this.received.add(received);
     }
 }
---
+++ src/main/java/ecse414/fall2015/group21/game/shared/connection/TCPConnectionHandler.java
@@ -0,0 +1,59 @@
+package ecse414.fall2015.group21.game.shared.connection;
+
+
+import ecse414.fall2015.group21.game.shared.data.Packet;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Handles what a TCP clint should do when it recieves a message from the server
+ */
+public class TCPConnectionHandler extends SimpleChannelInboundHandler<DatagramPacket>{
+    //Support concurrency since Netty I/O is asynchronous, or non-blocking
+    private final Queue<Packet.TCP> received = new ConcurrentLinkedQueue<>();
+
+
+
+    /**
+     * Is called for each message of type I
+     *
+     * @param ctx the {@link ChannelHandlerContext} which this {@link SimpleChannelInboundHandler}
+     *            belongs to
+     * @param msg the message to handle
+     * @throws Exception is thrown if an error occurred
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+        // Read the incoming message, convert to TCP packet using factory, and add to queue of recieved packets
+        Packet.TCP tcpPacket = Packet.TCP.FACTORY.newInstance(msg.content());
+        received.add(tcpPacket);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        //Flush the channel handle context when read is complete
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        // We don't close the channel because we can keep serving requests - is this still true for TCP?
+    }
+
+    //Read all recieved packets to a queue
+    public void readPackets(Queue<Packet.TCP> output){
+        while(received.peek() != null){
+            output.add(received.poll());
+        }
+
+    }
+
+
+
+}
